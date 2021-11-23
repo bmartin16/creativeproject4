220f1aa2f6df14fd166f93ce1f787d6271a15787
Blake Martin <18bmartin@gmail.com>
2021-11-23 09:28:53 -0700

    first commit

--- front-end/src/components/CartList.vue
+++ front-end/src/components/CartList.vue
@@ -101,8 +101,41 @@ button {
 }
 
 .auto {
+  position: relative;
+  background-color: #000;
+  border: none;
+  font-size: 28px;
+  color: #FFFFFF;
+  height: 50px;
+  width: 170px;
+  text-align: center;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  text-decoration: none;
+  overflow: hidden;
+  cursor: pointer;
   margin-left: auto;
+  margin-bottom: 10px;
 }
+.auto:after {
+  content: "";
+  background: #fefefe;
+  display: block;
+  position: absolute;
+  padding-top: 300%;
+  padding-left: 350%;
+  margin-left: -20px!important;
+  margin-top: -120%;
+  opacity: 0;
+  transition: all 0.8s
+}
+.auto:active:after {
+  padding: 0;
+  margin: 0;
+  opacity: 1;
+  transition: 0s
+}
+
 #leftArrow {
   background-image: url("/images/LeftArrow.jpg");
   width: 20%;
